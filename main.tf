8e82797d70f192a5adab660b07a6a5e1026d3e37
Patrick <82187582+patshash@users.noreply.github.com>
2023-08-03 16:56:02 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     random = {
-      source = "localterraform.com/hashicorp/random"
+      source = "localterraform.com/random"
       version = "3.5.1"
     }
   }
