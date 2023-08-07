5656c838f4ca00572f15c5f83a1d8b74c7adc080
Patrick <82187582+patshash@users.noreply.github.com>
2023-08-07 19:55:17 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     random = {
-      source = "tfe.pcarey.xyz/patrick/random"
+      source = "tfe.pcarey.xyz/myorg/random"
       version = "1.0.1"
     }
   }
